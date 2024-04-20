ff42b27705e836d9f6178c01f1bc240aacea0b59
Prateek Kumar <prateekptp@gmail.com>
2024-04-20 15:23:42 +0530

    added login, middleware and refactoring model methods

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-const express = require('express')
-const ejs = require('ejs');
-const path = require('path');
-const bcrypt = require('bcrypt');
+const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const User = require('./models/user');
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+
 
 mongoose.connect('mongodb://localhost:27017/authDemo')
     .then(() => {
-        console.log('Mongo Connection Open')
+        console.log("MONGO CONNECTION OPEN!!!")
     })
     .catch(err => {
-        console.log("Oh no Mongo Connection Error!")
-        console.log(err);
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
     })
 
-app.set('view engine', 'ejs')
-app.set('views', 'views');
-app.set(path.join(__dirname, 'views'));
 
-app.get('/', (req,res) => {
-    res.send("THIS IS THE HOME PAGE!");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret', resave: false, saveUninitialized: false }))
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    next();
+}
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE')
 })
 
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register', async (req,res) => {
-    const { username, password } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    })
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const user = new User({ username, password })
     await user.save();
-    res.redirect('/');
+    req.session.user_id = user._id;
+    res.redirect('/')
 })
 
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
     res.render('login')
 })
-
-app.post('/login', async (req,res) => {
-    const { username, password} = req.body;
-    const user = await User.findOne({ username })
-    const validPassword = await bcrypt.compare(password, user.password);
-    if(validPassword) {
-        res.send("Yay, Welcome!")
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
     }
     else {
-        res.send("TRY AGAIN!")
+        res.redirect('/login')
     }
 })
 
-app.get('/secret', (req,res) => {
-    res.send("This is Secret!");
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('/login');
 })
 
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret')
+})
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send("TOP SECRET!!!")
+})
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("SERVING YOUR APP!")
 })
+
